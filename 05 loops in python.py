a4ff272ea3067b204dd9be2e0e6d3e2185e04ae8
mzubair31102 <mzubair31102@gmail.com>
2022-06-09 22:08:11 +0500

    Python Tutorials Practice

---
+++ 05 loops in python.py
@@ -0,0 +1,29 @@
+# The while Loop
+# The break Statement
+# The continue Statement
+# The else Statement
+# For Loops
+# Looping Through a String
+# Looping Through a List
+# Looping Through a Tuple
+# Looping Through a Set
+# Looping Through a dictionary
+# The break Statement
+# The continue Statement
+# The range() Function
+# Else in For Loop
+# Nested Loops
+# The pass Statement
+# 1:  Write a program to print first 10 odd numbers.
+# 2: Write a program to print first 10 even numbers in reverse order.
+# 3: Write a program to print table of a number accepted from user.
+# 4: Write a program to find the factorial of a number.
+# 5: Write a program to check whether a number is prime or not.
+# 6: Write a program to print the following pattern
+"""
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+"""
